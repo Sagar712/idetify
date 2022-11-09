3cbe17bd5f567c66e87ba10b289e198c9e85e672
Sagar Wankhade <wankhadesagar761@gmail.com>
2022-11-09 10:27:39 +0530

    algoryth changes and ui change

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let data;
+let ranks = []
 
 main();
 async function main() {
@@ -15,6 +16,7 @@ function Identify_subject() {
     let query = document.querySelector("#query").value
     let result = fetch_subject_for_query(query)
     console.log(result);
+    console.log(showRanks());
     document.querySelector("#result").innerHTML = result
 }
 
@@ -24,8 +26,14 @@ function fetch_subject_for_query(query) {
     console.log(queryParts);
     let current_result = "unknown"
     for (let i = 0; i < queryParts.length; i++) {
-        const search_result = SearchSubject(queryParts[i])
-        console.log(search_result, typeof (search_result));
+        let search_result = SearchSubject(queryParts[i])
+        let search_result_slang_word = SearchSubject(queryParts[i], 'slang_subjects')
+
+        //Reranking between noraml vs slang words
+        if (search_result['match percentage'] < search_result_slang_word['match percentage']) {
+            search_result = search_result_slang_word
+        }
+        //console.log(search_result, typeof (search_result));
         if (search_result.category == "unknown") {
             current_result = `Category: ${search_result.category} | Closest word: ${search_result["closest word found"]} | Match percent: ${(search_result["match percentage"] * 100).toFixed(2)}%`
             continue
@@ -37,22 +45,30 @@ function fetch_subject_for_query(query) {
 }
 
 // Proper Subject indexing
-function SearchSubject(word) {
-    let all_keys = Object.keys(data['proper_subjects'])
+function SearchSubject(word, dataset = 'proper_subjects') {
+    let all_keys = Object.keys(data[dataset])
     let closet_word = "-"
     let similarity_percent = 0
     let highest_percent = 0
+    let current_key = "unknown"
+    let probable_key = "unknown"
     for (let j = 0; j < all_keys.length; j++) {
-        const current_key = all_keys[j];
-        let arr = data['proper_subjects'][current_key]
+        current_key = all_keys[j];
+        let arr = data[dataset][current_key]
         //console.log(arr);
         for (let i = 0; i < arr.length; i++) {
 
             similarity_percent = similarity(word, arr[i])
 
             if (similarity_percent > highest_percent) {
+                probable_key = current_key
                 closet_word = arr[i]
                 highest_percent = similarity_percent
+                reranker({
+                    category: probable_key,
+                    "closest word found": closet_word,
+                    "match percentage": highest_percent
+                })
             }
             if (word.toLowerCase().match(arr[i].toLowerCase())) {
                 return {
@@ -65,7 +81,7 @@ function SearchSubject(word) {
 
     }
     return {
-        category: "unknown",
+        category: probable_key,
         "closest word found": closet_word,
         "match percentage": highest_percent
     }
@@ -75,25 +91,39 @@ function SearchSubject(word) {
 function similarity(s1, s2) {
     s1 = s1.toLowerCase();
     s2 = s2.toLowerCase();
-    let bigger = s1.length > s2.length ? s1 : s2
-    let smaller = s1.length < s2.length ? s1 : s2
+
     let splitBig = s1.split("")
     let splitSmall = s2.split("")
 
     let match_count = 0
+    let reject = []
 
     for (let i = 0; i < splitBig.length; i++) {
         let current = splitBig[i]
         for (let j = 0; j < splitSmall.length; j++) {
             let current_2 = splitSmall[j]
-            if (current === current_2) {
+            if (current === current_2 && match_count < splitBig.length && match_count < splitSmall.length && !reject.includes(j)) {
                 match_count++
+                reject.push(j)
                 break
             }
         }
-
     }
 
-    return (match_count / (splitBig.length - match_count + splitSmall.length))
+    let denominator = splitBig.length + splitSmall.length - match_count
+    //console.log("matches: "+match_count," out of: "+denominator);
+    return (match_count / denominator)
+}
+
+function reranker(elem) {
+    ranks.push(elem)
+    if (ranks.length > 5)
+        ranks.splice(0, 1)
+}
+
+function showRanks() {
+    let duplicate = ranks.slice()
+
+    return duplicate.reverse()
 }
 
--- trial.js
+++ trial.js
@@ -1,27 +1,26 @@
-console.log("Hii");
+let arr = []
+reranker(30)
+reranker(40)
+reranker(900)
+reranker(00)
+reranker(90)
+reranker(99)
 
-let str1 = "kapde"
-let str2 = "kapadee"
 
-let bigger = str1.length>str2.length ? str1 : str2
-let smaller = str1.length<str2.length ? str1 : str2
-
-let splitBig = bigger.split("")
-let splitSmall = smaller.split("")
-console.log(splitBig,splitSmall);
-
-let match_count = 0
-for (let i = 0; i < splitBig.length; i++) {
-    let current = splitBig[i]
-    for (let j = 0; j < splitSmall.length; j++) {
-        let current_2 = splitSmall[j]
-        if(current === current_2){
-            match_count++
-            break
-        }
-    }
-    
+function reranker(elem) {
+    arr.push(elem)
+    if(arr.length>5)
+        arr.splice(0,1)
 }
-console.log("Matches:"+ match_count);
-console.log("Match percent:"+ match_count,splitBig.length, splitSmall.length);
-console.log("Match percent:"+ (match_count/(match_count+(splitBig.length-match_count)+(splitSmall.length-match_count)))*100+ " %");
+
+function showRanks() {
+    let duplicate = arr.slice()
+    
+    return duplicate.reverse()
+}
+
+
+
+//reranker(99)
+
+console.log(arr, showRanks());
