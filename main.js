8da24b9843dd977e0373eb56433539ce8fb80c75
Sagar Wankhade <wankhadesagar761@gmail.com>
2022-11-11 00:40:45 +0530

    sequence service enabled

--- main.js
+++ main.js
@@ -17,13 +17,26 @@ function Identify_subject() {
     let result = fetch_subject_for_query(query)
     console.log(result);
     console.log(showRanks());
+
+
+    let k = 0, result_str = ''
+    let all_ranks = showRanks()
+    while (all_ranks[k] != undefined) {
+        result_str += create_row(all_ranks[k].category, all_ranks[k]["closest word found"], (all_ranks[k]["match percentage"] * 100).toFixed(2))
+        k++
+    }
+
     document.querySelector("#result").innerHTML = `<table> 
-    <tr> <td>Category</td> <td>Closest word</td> <td>Match</td> </tr>
-    <tr> <td class='special'>${result.category}</td> <td class='special'>${result["closest word found"]}
-        </td> <td class='special'>${(result["match percentage"] * 100).toFixed(2)} %</td> </tr>
+    ${create_row('Category', 'Closest word', 'Match', 1)}
+    ${result_str}
     </table>`
-    
-    result
+}
+
+function create_row(a, b, c, spl) {
+    if (spl == 1)
+        return `<tr> <td> ${a} </td> <td> ${b} </td> <td> ${c} </td> </tr>`
+    else
+        return `<tr> <td class='special'> ${a} </td> <td class='special'> ${b} </td> <td class='special'> ${c} %</td> </tr>`
 }
 
 function fetch_subject_for_query(query) {
@@ -31,7 +44,7 @@ function fetch_subject_for_query(query) {
     let queryParts = query.split(" ")
     console.log(queryParts);
     let current_result = "unknown"
-    let last_result = {'match percentage':0}
+    let last_result = { 'match percentage': 0 }
     ranks = []
     for (let i = 0; i < queryParts.length; i++) {
         let search_result = SearchSubject(queryParts[i])
@@ -42,7 +55,7 @@ function fetch_subject_for_query(query) {
             search_result = search_result_slang_word
         }
 
-        if(last_result["match percentage"] < search_result['match percentage']){
+        if (last_result["match percentage"] < search_result['match percentage']) {
             last_result = search_result
         }
         //console.log(search_result, typeof (search_result));
@@ -61,49 +74,44 @@ function fetch_subject_for_query(query) {
 // Proper Subject indexing
 function SearchSubject(word, dataset = 'proper_subjects') {
     let all_keys = Object.keys(data[dataset])
-    let closet_word = "-"
+    let closest_word = "-"
     let similarity_percent = 0
     let highest_percent = 0
     let current_key = "unknown"
     let probable_key = "unknown"
+    //iterating through each category
     for (let j = 0; j < all_keys.length; j++) {
         current_key = all_keys[j];
         let arr = data[dataset][current_key]
         //console.log(arr);
+        let rank_percent = 0
+
+        //Iterating through each item of category, if we find higher match percent we add it to reranker service
         for (let i = 0; i < arr.length; i++) {
 
             similarity_percent = similarity(word, arr[i])
 
             if (similarity_percent > highest_percent) {
                 probable_key = current_key
-                closet_word = arr[i]
-                let rank_percent = highest_percent
+                closest_word = arr[i]
                 highest_percent = similarity_percent
-                if (rank_percent < highest_percent)
-                    reranker({
-                        category: probable_key,
-                        "closest word found": closet_word,
-                        "match percentage": highest_percent
-                    })
             }
-            // if (word.toLowerCase() === (arr[i].toLowerCase())) {
-            //     return {
-            //         category: current_key,
-            //         "closest word found": closet_word,
-            //         "match percentage": highest_percent
-            //     }
-            // }
+            reranker({
+                category: current_key,
+                "closest word found": arr[i],
+                "match percentage": similarity_percent
+            })
         }
 
     }
     return {
         category: probable_key,
-        "closest word found": closet_word,
+        "closest word found": closest_word,
         "match percentage": highest_percent
     }
 }
 
-//Similarity calculation
+//Similarity calculation service
 function similarity(s1, s2) {
     s1 = s1.toLowerCase();
     s2 = s2.toLowerCase();
@@ -128,16 +136,27 @@ function similarity(s1, s2) {
 
     let denominator = splitBig.length + splitSmall.length - match_count
     //console.log("matches: "+match_count," out of: "+denominator);
-    return (match_count / denominator)
+    return ((match_count / denominator) + sequence_match(s2, s1))/2
 }
 
+//Reranker service
 function reranker(elem) {
+    for (let i = 0; i < ranks.length; i++) {
+        if (ranks[i] != undefined && ranks[i + 1] != undefined) {
+            if (elem['match percentage'] > ranks[i]['match percentage'] && elem['match percentage'] <= ranks[i + 1]['match percentage']) {
+                ranks.splice(i + 1, 0, elem)
+                break
+            }
+        }
+        else if (ranks[i] != undefined) {
+            if (elem['match percentage'] > ranks[i]['match percentage']) {
+                ranks.push(elem)
+                break
+            }
+        }
+    }
     if (ranks.length == 0)
         ranks.push(elem)
-    else{
-        if(elem['match percentage'] > ranks[ranks.length-1]['match percentage'])
-            ranks.push(elem)
-    }
     if (ranks.length > 5)
         ranks.splice(0, 1)
 }
@@ -147,3 +166,68 @@ function showRanks() {
     return duplicate.reverse()
 }
 
+
+//Sequence service:
+function sequence_match(base, test) {
+
+    base = base.toLowerCase()
+    test = test.toLowerCase()
+    let base_split = base.split('')
+    let test_split = test.split('')
+    let score = 0
+
+    for (let i = 0; i < base_split.length; i++) {
+
+        let temp1 = 0.9, temp2 = 0.9, found = false
+
+        if (test_split[i] != undefined) {
+            if (base_split[i] == test_split[i])
+                score++
+            else {
+                for (let k = i - 1; k >= 0; k--) {
+                    if (base_split[k] == test_split[i]) {
+                        found = true
+                        break
+                    }
+                    else{
+                        temp1 = temp1 - 0.3
+                        temp1 = parseFloat(temp1.toFixed(2))
+                    }
+
+                    if (temp1 < 0)
+                        break
+                }
+                if (!found)
+                    temp1 = 0
+
+                found = false
+                for (let k = i + 1; k < base_split.length; k++) {
+                    if (base_split[k] == test_split[i]) {
+                        found = true
+                        break
+                    }
+                    else{
+                        temp2 = temp2 - 0.3
+                        temp2 = parseFloat(temp2.toFixed(2))
+                    }
+
+                    if (temp2 < 0)
+                        break
+                }
+                if (!found)
+                    temp2 = 0
+
+                if (temp1 >= temp2)
+                    score = score + temp1
+                else
+                    score = score + temp2
+            }
+        }
+        else
+            break
+    }
+
+    //return score
+    return score/base_split.length
+    //console.log(base_split, test_split);
+}
--- trial.js
+++ trial.js
@@ -1,34 +1,99 @@
-// let arr = []
-// reranker(30)
-// reranker(40)
-// reranker(900)
-// reranker(00)
-// reranker(90)
-// reranker(99)
-// arr = []
+// let str1 = 'ghetle'
+// let str2 = 'ghltee'
 
+// function sequence_match(base, test) {
 
-// function reranker(elem) {
-//     if (arr.length == 0)
-//         arr.push(elem)
-//     else{
-//         if(elem > arr[arr.length-1])
-//             arr.push(elem)
+//     base = base.toLowerCase()
+//     test = test.toLowerCase()
+//     let base_split = base.split('')
+//     let test_split = test.split('')
+//     let score = 0
+
+//     for (let i = 0; i < base_split.length; i++) {
+
+//         let temp1 = 0.9, temp2 = 0.9, found = false
+
+//         if (test_split[i] != undefined) {
+//             if (base_split[i] == test_split[i])
+//                 score++
+//             else {
+//                 for (let k = i - 1; k >= 0; k--) {
+//                     if (base_split[k] == test_split[i]) {
+//                         found = true
+//                         break
+//                     }
+//                     else{
+//                         temp1 = temp1 - 0.3
+//                         temp1 = parseFloat(temp1.toFixed(2))
+//                     }
+
+//                     if (temp1 < 0)
+//                         break
+//                 }
+//                 if (!found)
+//                     temp1 = 0
+
+//                 found = false
+//                 for (let k = i + 1; k < base_split.length; k++) {
+//                     if (base_split[k] == test_split[i]) {
+//                         found = true
+//                         break
+//                     }
+//                     else{
+//                         temp2 = temp2 - 0.3
+//                         temp2 = parseFloat(temp2.toFixed(2))
+//                     }
+
+//                     if (temp2 < 0)
+//                         break
+//                 }
+//                 if (!found)
+//                     temp2 = 0
+
+//                 if (temp1 >= temp2)
+//                     score = score + temp1
+//                 else
+//                     score = score + temp2
+//             }
+//         }
+//         else
+//             break
 //     }
-//     if (arr.length > 5)
-//         arr.splice(0, 1)
+
+//     //return score
+//     return 'sequence match: '+(score/base_split.length).toFixed(2)*100+' %'
+//     //console.log(base_split, test_split);
 // }
 
-// function showRanks() {
-//     let duplicate = arr.slice()
+// function similarity(s1, s2) {
+//     s1 = s1.toLowerCase();
+//     s2 = s2.toLowerCase();
 
-//     return duplicate.reverse()
+//     let splitBig = s1.split("")
+//     let splitSmall = s2.split("")
+
+//     let match_count = 0
+//     let reject = []
+
+//     for (let i = 0; i < splitBig.length; i++) {
+//         let current = splitBig[i]
+//         for (let j = 0; j < splitSmall.length; j++) {
+//             let current_2 = splitSmall[j]
+//             if (current === current_2 && match_count < splitBig.length && match_count < splitSmall.length && !reject.includes(j)) {
+//                 match_count++
+//                 reject.push(j)
+//                 break
+//             }
+//         }
+//     }
+
+//     let denominator = splitBig.length + splitSmall.length - match_count
+//     //console.log("matches: "+match_count," out of: "+denominator);
+//     return 'words match: '+(match_count / denominator).toFixed(2)*100+' %'
 // }
 
+// console.log(str1+' <-> '+str2);
 
+// console.log(similarity(str1, str2));
 
-// //reranker(99)
-
-// console.log(arr, showRanks());
-
-// console.log(arr[arr.length-1]);
+// console.log(sequence_match(str1, str2));
